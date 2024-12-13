ec0f78c2e251513298e147d8789af1d96aae4881
Vithushan <sutharshanvithushan@gmail.com>
2024-12-13 14:03:33 +0530

    fix: colorscheme.sh

--- colorscheme.sh
+++ colorscheme.sh
@@ -8,16 +8,16 @@ sed -i "s/$CURRENTTHEME/$NEWTHEME/g" ~/.config/rofi/config.rasi
 sed -i "s/\".*$CURRENTTHEME.*\"/\"$NEWTHEME\"/g" ~/.config/nvim/after/plugin/colors.lua
 
 mv ~/.config/dunst/dunstrc ~/.config/dunst/dunstrc.bak
-cp ~/.config/dunst/$NEWTHEME ~/.config/dunst/dunstrc
+cp ~/.config/dunst/$NEWTHEME ~/.config/dunst/dunstrc ||  mv ~/.config/dunst/dunstrc.bak ~/.config/dunst/dunstrc
 
 mv ~/.config/qtile/config/colors.json ~/.config/qtile/config/colors.json.bak
-cp ~/.config/qtile/config/$NEWTHEME.json ~/.config/qtile/config/colors.json
+cp ~/.config/qtile/config/$NEWTHEME.json ~/.config/qtile/config/colors.json || mv ~/.config/qtile/config/colors.json.bak ~/.config/qtile/config/colors.json
 
 [ ! -d ~/.config/starship ] && mkdir -p ~/.config/starship 
 mv ~/.config/starship.toml ~/.config/starship/starship.toml.bak
-cp ~/.config/starship/$NEWTHEME.toml ~/.config/starship.toml
+cp ~/.config/starship/$NEWTHEME.toml ~/.config/starship.toml || mv ~/.config/starship.toml.bak ~/.config/starship/starship.toml
 
 mv ~/.config/zathura/zathurarc ~/.config/zathura/zathurarc.bak
-cp ~/.config/zathura/$NEWTHEME ~/.config/zathura/zathurarc
+cp ~/.config/zathura/$NEWTHEME ~/.config/zathura/zathurarc || mv ~/.config/zathura/zathurarc.bak ~/.config/zathura/zathurarc
 
-echo "Colorscheme changed from $CURRENTTHEME to $NEWTHEME"
+echo "Colorscheme changed from $CURRENTTHEME to $NEWTHEME."
