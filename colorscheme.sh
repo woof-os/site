272d950630f4f0538eb8eecb9ba2028f7b2d0f02
Vithushan <sutharshanvithushan@gmail.com>
2024-12-14 05:55:04 +0530

    style: update script

--- colorscheme.sh
+++ colorscheme.sh
@@ -8,9 +8,8 @@ NEWTHEME=$2
 echo "
 NOTE: This script assumes that you're using the default Woof OS configuration
 files, and that you have downloaded your preferred colorscheme to the correct
-directories."
+directories.
 
-echo "
 =============================================================================
 "
 
@@ -19,16 +18,16 @@ sed -i "s/$CURRENTTHEME/$NEWTHEME/g" ~/.config/rofi/config.rasi
 sed -i "s/\".*$CURRENTTHEME.*\"/\"$NEWTHEME\"/g" ~/.config/nvim/after/plugin/colors.lua
 
 mv ~/.config/dunst/dunstrc ~/.config/dunst/dunstrc.bak
-cp ~/.config/dunst/$NEWTHEME ~/.config/dunst/dunstrc ||  mv ~/.config/dunst/dunstrc.bak ~/.config/dunst/dunstrc
+cp ~/.config/dunst/$NEWTHEME ~/.config/dunst/dunstrc 
 
 mv ~/.config/qtile/config/colors.json ~/.config/qtile/config/colors.json.bak
-cp ~/.config/qtile/config/$NEWTHEME.json ~/.config/qtile/config/colors.json || mv ~/.config/qtile/config/colors.json.bak ~/.config/qtile/config/colors.json
+cp ~/.config/qtile/config/$NEWTHEME.json ~/.config/qtile/config/colors.json
 
 [ ! -d ~/.config/starship ] && mkdir -p ~/.config/starship 
 mv ~/.config/starship.toml ~/.config/starship/starship.toml.bak
-cp ~/.config/starship/$NEWTHEME.toml ~/.config/starship.toml || mv ~/.config/starship.toml.bak ~/.config/starship/starship.toml
+cp ~/.config/starship/$NEWTHEME.toml ~/.config/starship.toml
 
 mv ~/.config/zathura/zathurarc ~/.config/zathura/zathurarc.bak
-cp ~/.config/zathura/$NEWTHEME ~/.config/zathura/zathurarc || mv ~/.config/zathura/zathurarc.bak ~/.config/zathura/zathurarc
+cp ~/.config/zathura/$NEWTHEME ~/.config/zathura/zathurarc
 
 echo "Colorscheme changed from $CURRENTTHEME to $NEWTHEME."
