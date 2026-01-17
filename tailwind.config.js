ad74d158f8524805995a5f1f5009e4770d72d10d
Vithushan <sutharshanvithushan@gmail.com>
2026-01-18 00:03:07 +0530

    background:

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
     extend: {
       backgroundImage: {
         // main: 'url("https://raw.githubusercontent.com/woof-os/wallpapers/main/drawn/woof.png")',
-        main: 'url("https://lewoof.xyz/assets/images/background.jpg")',
+        main: 'url("/images/background.jpg")',
       },
       backgroundSize: {
         main: "cover",
